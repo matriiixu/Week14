2407f9b3b3712ef1e542d36ebbe77d222a45506d
matriiixu <oani1_16@uni.worc.ac.uk>
2018-02-06 20:00:35 +0000

    Update 1.2

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -1,38 +1,48 @@
-Backendless.initApp("5635B4D5-C5C6-89F0-FF31-CCF45BCE9900","7E1F94C4-523F-48B0-FFB6-4BAEBE6A2B00");
-var app = {
-    // CONSTRUCTOR
-    initialize: function() {
-        this.bindEvents();
-    },
+Backendless.initApp("5635B4D5-C5C6-89F0-FF31-CCF45BCE9900","7100DD0F-81D1-52F7-FF6A-0B85A153D400");
+
+// When page opens
+$(document).on("pageshow","#pageone", onPageShow);
+
+function onPageShow() {
     
-    // EVENT BINDER
-    bindEvents: function() {
-        
-        
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
+    Backendless.Data.of("Tasks").find().then(processResults).catch(error);
     
-    // DEVICEREADY
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    
-    // UPDATE
-    receivedEvent: function(id) {
-        
-        
-        
-        Backendless.Data.of("TASKS").find().then(processResults).catch(error);
-        function processResults(tasks) {
+    function processResults(tasks) {
         //display the first task in an array of tasks.
-            var text = document.getElementById('text');
-            text.innerHTML=tasks[0].Task;
-            //alert();
+            
+        for(var i=0; i<tasks.length; i++){
+            var number = i + 1;
+            $('#tasklist').append("<li>" + number +") " + "<nbsp>" + tasks[i].Task + "</li>");
+            console.log(number + ") " + tasks[i].Task);
         }
-        function error(err) {
-        alert(err);
-        }
-        
-        
+        $('#tasklist').listview('refresh');
+    
     }
-};
+    function error(err) {
+        console.log(err);
+    }
+}
+
+// When add is clicked
+$(document).on("click","#addTaskButton", onAddTask);
+    
+    function onAddTask(){
+        var tasktext = $('#addTaskText').val();
+        if(tasktext != ""){
+            console.log('Add Button Pressed, here is what you\'ve written: ');
+            console.log('\"' + tasktext + '\"');
+
+            var newTask = {};
+            newTask.Task = tasktext;
+            Backendless.Data.of("Tasks").save(newTask).then(saved).catch(error);
+
+            function saved(savedTask) {
+                console.log( "new Contact instance has been saved" + savedTask);
+            }
+            
+        }else{
+            alert('Write a task');
+        }
+    }
+
+$(document).on("click","#addTaskButton", onAddTask);
