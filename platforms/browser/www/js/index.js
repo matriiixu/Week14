8919119aab2a8f7a770fa01204887f7d11077446
matriiixu <oani1_16@uni.worc.ac.uk>
2018-02-06 12:52:07 +0000

    Update 1.1

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -1,3 +1,4 @@
+Backendless.initApp("5635B4D5-C5C6-89F0-FF31-CCF45BCE9900","7E1F94C4-523F-48B0-FFB6-4BAEBE6A2B00");
 var app = {
     // CONSTRUCTOR
     initialize: function() {
@@ -6,6 +7,8 @@ var app = {
     
     // EVENT BINDER
     bindEvents: function() {
+        
+        
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     
@@ -16,7 +19,20 @@ var app = {
     
     // UPDATE
     receivedEvent: function(id) {
-        var text = document.getElementById('text');
-        text.innerHTML='Hi, this is update!';
+        
+        
+        
+        Backendless.Data.of("TASKS").find().then(processResults).catch(error);
+        function processResults(tasks) {
+        //display the first task in an array of tasks.
+            var text = document.getElementById('text');
+            text.innerHTML=tasks[0].Task;
+            //alert();
+        }
+        function error(err) {
+        alert(err);
+        }
+        
+        
     }
 };
